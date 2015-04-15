7bc8bf7d465f4a75364ed0395bcfd3919916f0f7
John Co-Reyes <jcoreyes@caltech.edu>
2015-04-15 00:20:02 -0700

    Added comments and answered questions

--- hw2/transpose_cuda.cu
+++ hw2/transpose_cuda.cu
@@ -35,7 +35,6 @@
  */
 __global__
 void naiveTransposeKernel(const float *input, float *output, int n) {
-  // TODO: do not modify code, just comment on suboptimal accesses
 
   const int i = threadIdx.x + 64 * blockIdx.x;
   int j = 4 * threadIdx.y + 64 * blockIdx.y;
@@ -43,44 +42,65 @@ void naiveTransposeKernel(const float *input, float *output, int n) {
 
   /*
   Each warp handles a 32 x 4 submatrix and each thread in a warp
-  handles a column of this submatrix. So a warp access 32 different
-  columns. Thus the read is not coalesced since a warp will access
-  32 different cache lines.
+  handles a column of this submatrix. So a warp reads from 32 consecutive
+  columns one row per instruction. So assuming the input matrix is in major 
+  format then the read is coalesced. The write is not coalesced however since
+  each warp writes to 32 consecutive rows, one column per instruction. So
+  a warp writes to 32 different 128 byte cache lines.
    */
   for (; j < end_j; j++) {
     output[j + n * i] = input[i + n * j];
   }
 }
 
+/**
+ * We will use shared memory to stored the 64x64 matrix per block. Then a warp
+ * will read in a row from input and write it in as a column to shared memory.
+ * We want to read in a row at a time so the read is coalesced. We will
+ * padd the shared memory to avoid memory bank conflicts. Then a warp
+ * will read in a a row from shared memory and write to a row into output
+ * using the correct transposed indices. We want to again write in a row
+ * to so the write is coalesced.
+ */
 __global__
 void shmemTransposeKernel(const float *input, float *output, int n) {
-  // TODO: Modify transpose kernel to use shared memory. All global memory
-  // reads and writes should be coalesced. Minimize the number of shared
-  // memory bank conflicts (0 bank conflicts should be possible using
-  // padding). Again, comment on all sub-optimal accesses.
 
   // Shared memory will store a 64x64 submatrix and be padded by a column at
   // the end since we will be accessing the shared memory stride 65 to avoid
   // memory bank conflicts
   __shared__ float data[65*64];
 
+  // Indices for reading from input
   const int i = threadIdx.x + 64 * blockIdx.x;
   int j = 4 * threadIdx.y + 64 * blockIdx.y;
   const int end_j = j + 4;
 
+  // Indices for writing to output
   const int i_t = threadIdx.x + 64 * blockIdx.y;
   int j_t = 4 * threadIdx.y + 64 * blockIdx.x;
   const int end_j_t = j_t + 4;
 
+  // Indices for reading/writing to shared memory
   const int i_data = threadIdx.x;
   int j_data = 4 * threadIdx.y;
 
+  // Each warp will read from 32 consecutive columns, one row per
+  // instruction so reading from input is coalesced. In one 
+  // instruction, a warp reads a row and writes it to data as a 
+  // column. We avoid memory bank conflicts by padding the shared
+  // memory so that no two threads in a warp access the same memory
+  // bank.
   for (; j < end_j; j++) {
     data[j_data + 65*i_data] = input[i + n * j];
     j_data++;
   }
   __syncthreads();
   j_data -= 4;
+  // Each warp now reads a row from shared memory so there
+  // are not memory bank conflicts. We write the row as a 
+  // row into output using the correct transposed indices.
+  // A warp writes 32 consecutive columns, one row per instruction
+  // so the write is coalesced.
   for (; j_t < end_j_t; j_t++) {
     output[i_t + n * j_t] = data[i_data + 65 * j_data];
     j_data++;
@@ -90,24 +110,28 @@ void shmemTransposeKernel(const float *input, float *output, int n) {
 
 __global__
 void optimalTransposeKernel(const float *input, float *output, int n) {
-  // TODO: This should be based off of your shmemTransposeKernel.
-  // Use any optimization tricks discussed so far to improve performance.
   __shared__ float data[65*64];
 
+  // Move all constants calculation to the beginning
+  // These are independent from each other so we can
+  // perform all of them at the same time
   const int i = threadIdx.x + 64 * blockIdx.x;
   const int j = 4 * threadIdx.y + 64 * blockIdx.y;
-
 
   const int i_data = threadIdx.x;
   const int j_data = 4 * threadIdx.y;
 
   const int i_t = threadIdx.x + 64 * blockIdx.y;
   const int j_t = 4 * threadIdx.y + 64 * blockIdx.x;
+
+  // Unroll for loop to avoid instruction dependencies
   data[j_data + 65*i_data] = input[i + n * j];
   data[j_data + 1 + 65*i_data] = input[i + n * (j+1)];
   data[j_data + 2 + 65*i_data] = input[i + n * (j+2)];
   data[j_data + 3 + 65*i_data] = input[i + n * (j+3)];
   __syncthreads();
+
+  // Unroll for loop to avoid instruction dependencies
   output[i_t + n * j_t] = data[i_data+ 65 * j_data];
   output[i_t + n * (j_t+1)] = data[i_data+ 65 * (j_data+1)];
   output[i_t + n * (j_t+2)] = data[i_data+ 65 * (j_data+2)];
